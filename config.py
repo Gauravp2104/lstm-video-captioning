3f315c84fa90cfb775f32a16af0b716b9e678bf8
Gauravp2104 <100126824+Gauravp2104@users.noreply.github.com>
2024-11-14 18:39:07 +0530

    First upload

---
+++ config.py
@@ -0,0 +1,15 @@
+train_path = "data/training_data"
+test_path = "data/testing_data"
+batch_size = 2
+learning_rate = 0.0007
+epochs = 10
+latent_dim = 512
+num_encoder_tokens = 4096
+num_decoder_tokens = 4096
+time_steps_encoder = 80
+time_steps_decoder = 9
+max_probability = -1
+save_model_path = 'model_final/'
+validation_split = 0.15
+max_length = 10
+search_type = 'greedy'
